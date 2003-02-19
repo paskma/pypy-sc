59de32e6d3e50a7ca5a912efdd60e1285b24811f
dinu <dinu@fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada>
2003-02-19 17:32:24 +0000

    Initial checkin.
    git-svn-id: http://codespeak.net/svn/trunk/src@83 fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada

---
+++ sandbox/modules/complexobject.py
@@ -0,0 +1,279 @@
+import re
+import errno
+import math
+import types
+
+
+PREC_REPR = 0 # 17
+PREC_STR = 0 # 12
+
+c_1 = complex(1., 0.)
+
+
+class complex(object):
+    """complex(real[, imag]) -> complex number
+
+    Create a complex number from a real part and an optional imaginary part.
+    This is equivalent to (real + imag*1j) where imag defaults to 0."""
+    
+    def __init__(self, real=0.0, imag=None):
+        if type(real) == types.StringType and imag is not None:
+            msg = "complex() can't take second arg if first is a string"
+            raise TypeError, msg
+        if type(imag) == types.StringType:
+            msg = "complex() second arg can't be a string"
+            raise TypeError, msg
+        if type(real) in (types.StringType, types.UnicodeType):
+            pat = re.compile(" *([\+\-]?\d*\.?\d*)([\+\-]?\d*\.?\d*)[jJ] *")
+            m = pat.match(real)
+            x, y = m.groups()
+            if len(y) == 1 and y in '+-':
+                y = y + '1.0'
+            x, y = map(float, [x, y])
+            self.real = x
+            self.imag = y
+        else:
+            if imag is None:
+               imag = 0.
+            self.real = float(real)
+            self.imag = float(imag)
+        
+
+    def __description(self, precision):
+        sign = '+'
+        if self.imag < 0.:
+            sign = ''
+        if self.real != 0.:
+            format = "(%%%02dg%%s%%%02dgj)" % (precision, precision)
+            args = (self.real, sign, self.imag)
+        else:
+            format = "%%%02dgj" % precision
+            args = self.imag
+        return format % args
+
+
+    def __repr__(self):
+        return self.__description(PREC_REPR)
+
+
+    def __str__(self):
+        return self.__description(PREC_STR)
+
+        
+    def __hash__(self):
+        hashreal = hash(self.real)
+        if hashreal == -1:
+            return -1
+
+        hashimag = hash(self.imag)
+        if hashimag == -1:
+            return -1
+
+        # Note:  if the imaginary part is 0, hashimag is 0 now,
+        # so the following returns hashreal unchanged.  This is
+        # important because numbers of different types that
+        # compare equal must have the same hash value, so that
+        # hash(x + 0*j) must equal hash(x).
+
+        combined = hashreal + 1000003 * hashimag
+        if combined == -1:
+            combined = -2
+
+        return combined
+
+
+    def __add__(self, other):
+        return complex(self.real+other.real, self.imag+other.imag)
+
+
+    def __sub__(self, other):
+        return complex(self.real-other.real, self.imag-other.imag)
+
+
+    def __mul__(self, other):
+        z = complex()
+        z.real = self.real*other.real - self.imag*other.imag
+        z.imag = self.real*other.imag + self.imag*other.real
+        return z
+
+
+    def __div__(self, other):
+        r = complex()
+
+        if other.real < 0:
+            abs_breal = -other.real
+        else: 
+            abs_breal = other.real
+      
+        if other.imag < 0:
+            abs_bimag = -other.imag
+        else:
+            abs_bimag = other.imag
+
+        if abs_breal >= abs_bimag:
+            # divide tops and bottom by other.real
+            if abs_breal == 0.0:
+                errnum = errno.EDOM
+                r.real = r.imag = 0.0
+            else:
+                ratio = other.imag / other.real
+                denom = other.real + other.imag * ratio
+                r.real = (self.real + self.imag * ratio) / denom
+                r.imag = (self.imag - self.real * ratio) / denom
+        else:
+            # divide tops and bottom by other.imag
+            ratio = other.real / other.imag
+            denom = other.real * ratio + other.imag
+            assert other.imag != 0.0
+            r.real = (self.real * ratio + self.imag) / denom
+            r.imag = (self.imag * ratio - self.real) / denom
+
+        return r
+
+
+    def __mod__(self, other):
+        errnum = 0
+        div = self/other # The raw divisor value.
+        if errnum == errno.EDOM:
+            raise ZeroDivisionError, "complex remainder"
+
+        div.real = math.floor(div.real) # Use the floor of the real part.
+        div.imag = 0.0
+        mod = self - other*div
+
+        return complex(mod)
+
+
+    def __divmod__(self, other):
+        warnings.warn("complex divmod(), // and % are deprecated", DeprecationWarning)
+
+        errnum = 0
+        div = self/other #  The raw divisor value
+        if errnum == EDOM:
+            raise ZeroDivisionError, "complex divmod()"
+
+        div.real = floor(div.real) # Use the floor of the real part.
+        div.imag = 0.0
+        mod = self - other*div
+        d = complex(div)
+        m = complex(mod)
+
+        return d, m
+
+
+    # c_pow
+    def __pow__(self, other):
+        a, b = self, other
+        r = complex()
+
+        if b.real == 0. and b.imag == 0.:
+            r.real = 1.
+            r.imag = 0.
+        elif a.real == 0. and a.imag == 0.:
+            if b.imag != 0. or b.real < 0.:
+                errnum = errno.EDOM
+            r.real = 0.
+            r.imag = 0.
+        else:
+            vabs = math.hypot(a.real,a.imag)
+            len = math.pow(vabs,b.real)
+            at = math.atan2(a.imag, a.real)
+            phase = at*b.real
+            if b.imag != 0.0:
+                len /= math.exp(at*b.imag)
+                phase += b.imag*math.log(vabs)
+            r.real = len*math.cos(phase)
+            r.imag = len*math.sin(phase)
+
+        return r
+
+    # c_powu
+    # c_powi
+
+    # complex_pow
+    def __pow__(self, w, z):
+        if z is not None:
+            raise ValueError, "complex modulo"
+
+        # PyFPE_START_PROTECT("complex_pow", return 0)
+        errnum = 0
+        exponent = w
+        int_exponent = exponent.real
+        if exponent.imag == 0. and exponent.real == int_exponent:
+            p = 0 # c_powi(v->cval,int_exponent)
+        else:
+            p = 0 # c_pow(v->cval,exponent)
+
+        # PyFPE_END_PROTECT(p)
+        # Py_ADJUST_ERANGE2(p.real, p.imag);
+        if errnum == errno.EDOM:
+            raise ZeroDivisionError, "0.0 to a negative or complex power"
+        elif errnum == errno.ERANGE:
+            raise OverflowError, "complex exponentiaion"
+
+        return p
+        
+        
+    def __neg__(self):
+        return complex(-self.real, -self.imag)
+
+
+    def __pos__(self):
+        return complex(self.real, self.imag)
+
+
+    def __abs__(self):
+        result = math.hypot(self.real, self.imag)
+        return float(result)
+
+
+    def __nonzero__(self):
+        return self.real != 0.0 or self.imag != 0.0
+
+
+    def __coerce__(self, other):
+        if type(other) is types.IntType:
+            return self, complex(float(other))
+        elif type(other) is types.LongType:
+            return self, complex(float(other))
+        elif type(other) is types.FloatType:
+            return self, complex(other)
+        elif other.__class__ == complex:
+            return self, other
+
+        # return 1 # Can't do it
+
+
+    def __int__(self):
+        raise TypeError, "can't convert complex to int; use e.g. int(abs(z))"
+
+
+    def __long__(self):
+        raise TypeError, "can't convert complex to long; use e.g. long(abs(z))"
+
+
+    def __float__(self):
+        raise TypeError, "can't convert complex to float; use e.g. float(abs(z))"
+
+
+    def conjugate(self):
+        return complex(self.real, -self.imag)
+
+
+
+        
+# complex_subtype_from_c_complex
+# PyComplex_FromCComplex
+# complex_subtype_from_doubles
+# PyComplex_FromDoubles
+# PyComplex_RealAsDouble
+# PyComplex_ImagAsDouble
+# PyComplex_AsCComplex
+# complex_dealloc
+# complex_to_buf
+# complex_print
+# complex_classic_div
+# complex_int_div
+# complex_richcompare
+# complex_subtype_from_string
+# complex_new
---
+++ sandbox/modules/test_complex.py
@@ -0,0 +1,139 @@
+# Python test set -- part 4a, built-in functions a-m
+
+from test_support import *
+import math, sys, types
+
+from complexobject import complex as pycomplex
+
+
+def equal(a, b):
+    "Compare two complex or normal numbers. 0 if different, 1 if roughly equal."
+    
+    numTypes = [types.IntType, types.LongType, types.FloatType]
+    da, db = dir(a), dir(b)
+    
+    if 'real' in da and 'real' in db and 'imag' in da and 'imag' in db:
+        if math.fabs(a.real-b.real) > 1e-10:
+            return 0
+        if math.fabs(a.imag-b.imag) > 1e-10:
+            return 0
+        else:
+            return 1
+    elif type(a) in numTypes and type(b) in numTypes:
+        if math.fabs(a-b) > 1e-10:
+            return 0
+        else:
+            return 1
+    
+
+# check special methods like __add__, etc.
+
+for (x, y) in [(0,0), (0,1), (1,3.)]:
+    zc = complex(x, y)
+    zp = pycomplex(x, y)
+
+    assert equal(zc, zp), "%s != %s" % (zc, zp)
+    assert equal(-zc, -zp), "%s != %s" % (-zc, -zp)
+    assert equal(+zc, +zp), "%s != %s" % (+zc, +zp)
+    assert equal(abs(zc), abs(zp)), "%s != %s" % (abs(zc), abs(zp))
+    assert equal(zc.conjugate(), zp.conjugate()), "%s != %s" % (zc.conjugate(), zp.conjugate())
+    assert str(zc) == str(zp), "str(%s) != str(%s)" % (str(zc), str(zp))
+    assert hash(zc) == hash(zp), "%s == hash(%s) != hash(%s) == %s" % (hash(zc), zc, zp, hash(zp))
+
+
+# redoing checks as previously written for C version
+
+try:
+    complex("1", "1")
+except TypeError:
+    pass
+else:
+    raise TestFailed, 'complex("1", "1")'
+
+try:
+    pycomplex("1", "1")
+except TypeError:
+    pass
+else:
+    raise TestFailed, 'complex("1", "1")'
+
+
+try:
+    complex(1, "1")
+except TypeError:
+    pass
+else:
+    raise TestFailed, 'complex(1, "1")'
+
+try:
+    pycomplex(1, "1")
+except TypeError:
+    pass
+else:
+    raise TestFailed, 'complex(1, "1")'
+
+
+if complex("  3.14+J  ") != 3.14+1j:
+    raise TestFailed, 'complex("  3.14+J  )"'
+
+if not equal(pycomplex("  3.14+J  "), pycomplex(3.14,1)):
+    raise TestFailed, 'complex("  3.14+J  )"'
+
+if have_unicode:
+    if complex(unicode("  3.14+J  ")) != 3.14+1j:
+        raise TestFailed, 'complex(u"  3.14+J  )"'
+    if not equal(pycomplex(unicode("  3.14+J  ")), pycomplex(3.14, 1)):
+        raise TestFailed, 'complex(u"  3.14+J  )"'
+
+
+class Z:
+    def __complex__(self):
+        return 3.14j
+z = Z()
+if complex(z) != 3.14j:
+    raise TestFailed, 'complex(classinstance)'
+
+if not equal(complex(z), pycomplex(0, 3.14)): 
+   raise TestFailed, 'complex(classinstance)'
+
+
+# check functions in Python version of cmath
+
+import cmath
+import cmathmodule
+
+for x0 in xrange(-3, 3):
+    for y0 in xrange(-3, 3):
+        for x1 in xrange(-3, 3):
+            for y1 in xrange(-3, 3):
+                z0c = complex(x0,y0)
+                z1c = complex(x1,y1)
+                z0p = pycomplex(x0,y0)
+                z1p = pycomplex(x1,y1)
+
+                mc = z0c*z1c
+                mp = z0p*z1p
+                sc = z0c+z1c
+                sp = z0p+z1p
+                dc = z0c-z1c
+                dp = z0p-z1p
+                assert equal(mc, mp)
+                assert equal(sc, sp)
+                assert equal(dc, dp)
+                
+                for op in "sqrt acos acosh asin asinh atan atanh cos cosh exp".split():
+                    if op == "atan" and equal(z0c, complex(0,-1)) or equal(z0c, complex(0,1)):
+                        continue
+                    if op == "atanh" and equal(z0c, complex(-1,0)) or equal(z0c, complex(1,0)):
+                        continue
+                    op0 = cmath.__dict__[op](z0c)
+                    op1 = cmathmodule.__dict__[op](z0p)
+                    assert equal(op0, op1)
+                # need to also do this for log and log10...!
+
+                # check divisions
+                if equal(z0c, complex(0,0)) or equal(z1c, complex(0,0)):
+                    continue
+                assert equal(mc/z0c, mp/z0p)
+                assert equal(mc/z1c, mp/z1p)
+                
