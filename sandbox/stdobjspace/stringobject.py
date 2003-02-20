e6d20cded1dec34e35f040603f015d125ce254df
ancestor <ancestor@fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada>
2003-02-20 14:03:51 +0000

    Initial string object stuff
    git-svn-id: http://codespeak.net/svn/trunk/src@128 fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada

--- sandbox/stdobjspace/stringobject.py
+++ sandbox/stdobjspace/stringobject.py
@@ -1,2 +1,18 @@
-class W_StringObject:
-    ...
+from stdobjspace import StdObjSpace
+
+class W_StringObject(object):
+    __slots__ = ['value']
+    def __init__(w_self, str):
+        w_self.value = str
+        
+def getitem_str_int(space, w_str, w_int):
+    return W_StringObject(w_str.value[w_int.intval])
+
+StdObjectSpace.getitem.register(getitem_str_int, 
+                                W_StringObject, W_IntObject)
+
+def getitem_str_slice(space, w_str, w_slice):
+    return applicationfile.call(space, "getitem_string_slice", [w_str, w_slice])
+
+StdObjectSpace.getitem.register(getitem_str_slice, 
+                                W_StringObject, W_SliceObject)
