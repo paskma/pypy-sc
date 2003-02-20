bf6db14cb5e708a5cf0fa4d20ec19f9e859fffba
tismer <tismer@fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada>
2003-02-20 13:49:21 +0000

    stepping towards integers
    git-svn-id: http://codespeak.net/svn/trunk/src@125 fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada

--- sandbox/stdobjspace/intobject.py
+++ sandbox/stdobjspace/intobject.py
@@ -15,10 +15,7 @@ class W_IntObject:
         return applicationfile.call(space, "int_getattr", [w_self, w_attrname])
 
 
-
-
-
-def int_add(space, w_int1, w_int2):
+def int_int_add(space, w_int1, w_int2):
     x = w_int1.intval
     y = w_int2.intval
     try:
@@ -26,6 +23,51 @@ def int_add(space, w_int1, w_int2):
     except OverflowError:
         raise FailedToImplement(OverflowError, "integer addition")
     return W_IntObject(z)
+
+def int_int_sub(space, w_int1, w_int2):
+    x = w_int1.intval
+    y = w_int2.intval
+    try:
+        z = x - y
+    except OverflowError:
+        raise FailedToImplement(OverflowError, "integer subtraction")
+    return W_IntObject(z)
+
+def int_int_mul(space, w_int1, w_int2):
+    x = w_int1.intval
+    y = w_int2.intval
+    try:
+        z = x * y
+    except OverflowError:
+        raise FailedToImplement(OverflowError, "integer multiplication")
+    return W_IntObject(z)
+
+def int_int_floordiv(space, w_int1, w_int2):
+    x = w_int1.intval
+    y = w_int2.intval
+    try:
+        z = x // y
+    except ZeroDivisionError:
+		raise   # we have to implemet the exception or it will be ignored
+    except OverflowError:
+        raise FailedToImplement(OverflowError, "integer division")
+    return W_IntObject(z)
+
+def int_int_truediv(space, w_int1, w_int2):
+    x = w_int1.intval
+    y = w_int2.intval
+    try:
+        z = x / y   # XXX make sure this is the new true division
+    except ZeroDivisionError:
+		raise   # we have to implemet the exception or it will be ignored
+    except OverflowError:
+        raise FailedToImplement(OverflowError, "integer division")
+    return W_IntObject(z)
+
+if 1 / 2 == 1 // 2:
+	int_int_div = int_int_floordiv
+else:
+	int_int_div = int_int_truediv
 
 StdObjSpace.add.register(int_add, W_IntObject, w_IntObject)
 
