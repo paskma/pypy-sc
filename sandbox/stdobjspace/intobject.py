e7b6fd5c418bc3eff0f158303448d09c23f9083d
arigo <arigo@fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada>
2003-02-20 13:04:02 +0000

    one more example
    git-svn-id: http://codespeak.net/svn/trunk/src@124 fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada

--- sandbox/stdobjspace/intobject.py
+++ sandbox/stdobjspace/intobject.py
@@ -1,3 +1,4 @@
+from objspace import *
 
 
 
@@ -12,3 +13,19 @@ class W_IntObject:
         #if space.is_true(w_result):
         #    return w_inttype
         return applicationfile.call(space, "int_getattr", [w_self, w_attrname])
+
+
+
+
+
+def int_add(space, w_int1, w_int2):
+    x = w_int1.intval
+    y = w_int2.intval
+    try:
+        z = x + y
+    except OverflowError:
+        raise FailedToImplement(OverflowError, "integer addition")
+    return W_IntObject(z)
+
+StdObjSpace.add.register(int_add, W_IntObject, w_IntObject)
+
