82c71e7b9fb06483eb25a97380f28dc2c2ddf59e
hpk <hpk@fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada>
2003-02-20 14:06:23 +0000

    added builtins test
    git-svn-id: http://codespeak.net/svn/trunk/src@131 fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada

---
+++ sandbox/interpreter/tests/test_interpreter.py
@@ -0,0 +1,27 @@
+import unittest
+
+from pyframe import PyFrame
+import trivialspace
+
+
+class TestInterpreter(unittest.TestCase):
+
+    def test_trivial1(self):
+
+        # build frame
+        space = trivialspace
+        bytecode = compile('def f(x): return x+1', '', 'exec').co_consts[0]
+        w_globals = space.wrap({'__builtins__': __builtins__})
+        w_locals = space.wrap({})
+        frame = PyFrame(space, bytecode, w_globals, w_locals)
+
+        # perform call
+        w_input = frame.space.wrap((5,))
+        frame.setargs(w_input)
+        w_output = frame.eval()
+        self.assertEquals(frame.space.unwrap(w_output), 6)
+
+        
+
+if __name__ == '__main__':
+    unittest.main()
