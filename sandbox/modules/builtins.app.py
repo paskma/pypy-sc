8412d2556e6556e16b216ecff171e244fc0bcc23
hpk <hpk@fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada>
2003-02-20 14:34:25 +0000

    removed from modules to make things easier
    git-svn-id: http://codespeak.net/svn/trunk/src@135 fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada

--- sandbox/modules/builtins.app.py
+++
@@ -1,7 +0,0 @@
-
-
-
-
-def map(function, list):
-    "docstring"
-    return [function(x) for x in list]
--- sandbox/modules/builtins.py
+++
@@ -1,6 +0,0 @@
-
-
-
-def r_chr(space, w_ascii):
-    w_character = space.newstring([w_ascii])
-    return w_character
