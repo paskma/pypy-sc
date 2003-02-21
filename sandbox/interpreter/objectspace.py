59579a53e7a5674fdbe461b5f099e7b7d2d165a0
arigo <arigo@fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada>
2003-02-21 13:28:00 +0000

    internal work & sharing files
    git-svn-id: http://codespeak.net/svn/trunk/src@190 fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada

--- sandbox/interpreter/objectspace.py
+++ sandbox/interpreter/objectspace.py
@@ -1,5 +1,69 @@
 import os
+import executioncontext, pyframe
 
+
+class OperationError(Exception):
+    """Interpreter-level exception that signals an exception that should be
+    sent to the application level.
+    
+    Arguments are the object-space exception class and value."""
+
+
+class NoValue(Exception):
+    """Raised to signal absence of value, e.g. in the iterator accessing
+    method 'iternext()' of object spaces."""
+
+
+
+class HelperBytecode:
+    def __init__(self, source, filename='<helper>'):
+        self.bytecode = compile(source, filename, 'exec')
+
+
+class AppFile(HelperBytecode):
+    """Dynamic loader of a set of Python functions and objects that
+    should work at the application level (conventionally in .app.py files)"""
+
+    # absolute name of the parent directory
+    ROOTDIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+    def __init__(self, filename):
+        "Load and compile the file."
+        # XXX looking for a pre-compiled file here will be quite essential
+        #     when we want to bootstrap the compiler
+        fullfn = os.path.join(AppFile.ROOTDIR, filename)
+        f = open(fullfn, 'r')
+        src = f.read()
+        f.close()
+        HelperBytecode.__init__(self, src, filename)
+
+
+class Helper:
+
+    def __init__(self, space, appfile):
+        "Send the helper module to the given object space."
+        self.space = space
+        # initialize the module by running the bytecode in a new
+        # dictionary, in a new execution context
+        ec = executioncontext.ExecutionContext(space)
+        self.w_namespace = ec.make_standard_w_globals()
+        frame = pyframe.PyFrame(space, appfile.bytecode,
+                                self.w_namespace, self.w_namespace)
+        ec.eval_frame(frame)
+
+    def get(self, objname):
+        "Returns a wrapped copy of an object by name."
+        w_name = space.wrap(objname)
+        w_obj = space.getitem(self.w_namespace, w_name)
+        return w_obj
+
+    def call(self, functionname, argumentslist):
+        w_function = self.get(space, functionname)
+        w_arguments = space.newtuple(argumentslist)
+        return space.apply(w_function, w_arguments)
+
+
+##################################################################
 
 class ObjectSpace:
     """Base class for the interpreter-level implementations of object spaces.
@@ -31,59 +95,4 @@ class ObjectSpace:
             self.appfile_helpers[applicationfile] = helper
         return helper
 
-
-
-class OperationError(Exception):
-    """Interpreter-level exception that signals an exception that should be
-    sent to the application level.
-    
-    Arguments are the object-space exception class and value."""
-
-
-class NoValue(Exception):
-    """Raised to signal absence of value, e.g. in the iterator accessing
-    method 'iternext()' of object spaces."""
-
-
-
-class AppFile:
-    """Dynamic loader of a set of Python functions and objects that
-    should work at the application level (conventionally in .app.py files)"""
-
-    # absolute name of the parent directory
-    ROOTDIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-    def __init__(self, filename):
-        "Load and compile the file."
-        # XXX looking for a pre-compiled file here will be quite essential
-        #     when we want to bootstrap the compiler
-        fullfn = os.path.join(AppFile.ROOTDIR, filename)
-        f = open(fullfn, 'r')
-        src = f.read()
-        f.close()
-        self.bytecode = compile(src, filename, 'exec')
-
-
-class Helper:
-
-    def __init__(self, space, appfile):
-        "Send the helper module to the given object space."
-        self.space = space
-        # initialize the module by running the bytecode in a new
-        # dictionary, in a new execution context
-        ec = executioncontext.ExecutionContext(space)
-        self.w_namespace = ec.make_standard_w_globals()
-        frame = pyframe.PyFrame(space, appfile.bytecode,
-                                self.w_namespace, self.w_namespace)
-        ec.eval_frame(frame)
-
-    def get(self, objname):
-        "Returns a wrapped copy of an object by name."
-        w_name = space.wrap(objname)
-        w_obj = space.getitem(self.w_namespace, w_name)
-        return w_obj
-
-    def call(self, functionname, argumentslist):
-        w_function = self.get(space, functionname)
-        w_arguments = space.newtuple(argumentslist)
-        return space.apply(w_function, w_arguments)
+    AppFile = AppFile   # make that class available for convenience
--- sandbox/interpreter/tests/test_interpreter.py
+++ sandbox/interpreter/tests/test_interpreter.py
@@ -2,42 +2,25 @@ import unittest, sys, os
 sys.path.insert(0, '..')
 
 from pyframe import PyFrame
-import trivialspace
+import objectspace, trivialspace, executioncontext
+
+
+def testcode(code, functionname, args):
+    helperbytecode = objectspace.HelperBytecode(code, '<test>')
+
+    space = trivialspace.TrivialSpace()
+    wrappedargs = [space.wrap(arg) for arg in args]
+    w_output = space.gethelper(helperbytecode).call(functionname, wrappedargs)
+    return space.unwrap(w_output)
 
 
 class TestInterpreter(unittest.TestCase):
 
-    def test_trivial1(self):
-        # build frame
-        space = trivialspace
-        bytecode = compile('def f(x): return x+1', '', 'exec').co_consts[0]
-        w_globals = space.wrap({'__builtins__': __builtins__})
-        w_locals = space.wrap({})
-        frame = PyFrame(space, bytecode, w_globals, w_locals)
-
-        # perform call
-        w_input = frame.space.wrap((5,))
-        frame.setargs(w_input)
-        w_output = frame.eval()
-        self.assertEquals(frame.space.unwrap(w_output), 6)
-
     def test_trivial_call(self):
-        # build frame
-        space = trivialspace
-        d = {}
-        exec '''
+        x = testcode('''
 def f(): return 42
-def g(): return f()''' in d 
-        w_globals = space.wrap(d)
-        w_locals = space.wrap({})
-        bytecode = d['g'].func_code
-        frame = PyFrame(space, bytecode, w_globals, w_locals)
-
-        # perform call
-        w_input = frame.space.wrap(())
-        frame.setargs(w_input)
-        w_output = frame.eval()
-        self.assertEquals(frame.space.unwrap(w_output), 42)
+def g(): return f()''', 'g', [])
+        self.assertEquals(x, 42)
 
 if __name__ == '__main__':
     unittest.main()
