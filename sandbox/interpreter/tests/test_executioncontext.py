2589f400c71cb9358ee78858d19899ab42eb12d7
mwh <mwh@fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada>
2003-02-21 11:24:29 +0000

    we'd like things to work like this
    git-svn-id: http://codespeak.net/svn/trunk/src@173 fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada

---
+++ sandbox/interpreter/tests/test_executioncontext.py
@@ -0,0 +1,27 @@
+import unittest, sys, os
+sys.path.insert(0, '..')
+
+from pyframe import PyFrame
+import trivialspace
+
+
+class TestExecutionContext(unittest.TestCase):
+
+    def test_trivial1(self):
+        # build frame
+        space = trivialspace
+
+        ec = ExecutionContext(space)
+        
+        bytecode = compile('def f(x): return x+1', '', 'exec').co_consts[0]
+        w_globals = space.wrap({'__builtins__': __builtins__})
+        w_locals = space.wrap({})
+        frame = PyFrame(space, bytecode, w_globals, w_locals)
+        w_input = frame.space.wrap((5,))
+        frame.setargs(w_input)
+        w_output = ec.eval_frame(frame)
+        self.assertEquals(frame.space.unwrap(w_output), 6)
+
+
+if __name__ == '__main__':
+    unittest.main()
