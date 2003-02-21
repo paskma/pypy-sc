58a84bb6883f2b82643f8c7240ce4a937ad3f485
mwh <mwh@fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada>
2003-02-21 16:57:37 +0000

    more tests
    git-svn-id: http://codespeak.net/svn/trunk/src@210 fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada

--- sandbox/interpreter/tests/test_interpreter.py
+++ sandbox/interpreter/tests/test_interpreter.py
@@ -16,12 +16,27 @@ def testcode(code, functionname, args):
 
 class TestInterpreter(unittest.TestCase):
 
+    def test_trivial(self):
+        x = testcode('''
+def g(): return 42''', 'g', [])
+        self.assertEquals(x, 42)
+
     def test_trivial_call(self):
         x = testcode('''
-print 42
 def f(): return 42
 def g(): return f()''', 'g', [])
         self.assertEquals(x, 42)
+
+    def test_trivial_call2(self):
+        x = testcode('''
+def f(): return 1 + 1
+def g(): return f()''', 'g', [])
+        self.assertEquals(x, 2)
+
+    def test_print(self):
+        x = testcode('''
+def g(): print 10''', 'g', [])
+        self.assertEquals(x, None)
 
 if __name__ == '__main__':
     unittest.main()
