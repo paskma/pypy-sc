fcc0ec7fc524d6ef878b38f9e15e2fd850425d07
tismer <tismer@fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada>
2003-02-22 20:57:31 +0000

    added test for restricted_int.py
    git-svn-id: http://codespeak.net/svn/trunk/src@295 fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada

--- pypy/objspace/std/restricted_int.py
+++ pypy/objspace/std/restricted_int.py
@@ -66,8 +66,10 @@ class r_int(int):
         return r_int(x % y)
 
     def __divmod__(self, other):
-        res = divmod(self, other)
-        return tuple(r_int(res[0]), r_int(res[1]))
+        x = int(self)
+        y = int(other)
+        res = divmod(x, y)
+        return (r_int(res[0]), r_int(res[1]))
 
     def __lshift__(self, n):
         # ensure long shift, so we don't depend on
@@ -116,7 +118,7 @@ class r_int(int):
     def __pos__(self):
         return r_int(self)
 
-    def __not__(self):
+    def __invert__(self):
         x = int(self)
         return r_int(~x)
 
@@ -154,62 +156,115 @@ class r_uint(long):
     def __new__(klass, val):
         return long.__new__(klass, val & klass._mask)
 
-    def __add__(x, y):
+    def __add__(self, other):
+        x = long(self)
+        y = long(other)
         return r_uint(x + y)
     __radd__ = __add__
     
-    def __sub__(x, y):
+    def __sub__(self, other):
+        x = long(self)
+        y = long(other)
         return r_uint(x - y)
-    __rsub__ = __sub__
+
+    def __rsub__(self, other):
+        y = long(self)
+        x = long(other)
+        return r_uint(x - y)
     
-    def __mul__(x, y):
+    def __mul__(self, other):
+        x = long(self)
+        y = long(other)
         return r_uint(x * y)
     __rmul__ = __mul__
 
-    def __div__(x, y):
+    def __div__(self, other):
+        x = long(self)
+        y = long(other)
         return r_uint(x // y)
-    __rdiv__ = __div__
 
-    __floordiv__ = __rfloordiv__ = __div__
+    __floordiv__ = __div__
 
-    def __mod__(x, y):
+    def __rdiv__(self, other):
+        y = long(self)
+        x = long(other)
+        return r_uint(x // y)
+
+    __rfloordiv__ = __rdiv__
+
+    def __mod__(self, other):
+        x = long(self)
+        y = long(other)
         return r_uint(x % y)
-    __rmod__ = __mod__
 
-    def __divmod__(x, y):
+    def __rmod__(self, other):
+        y = long(self)
+        x = long(other)
+        return r_uint(x % y)
+
+    def __divmod__(self, other):
+        x = long(self)
+        y = long(other)
         res = divmod(x, y)
-        return tuple(r_uint(res[0]), r_uint(res[1]))
+        return (r_uint(res[0]), r_uint(res[1]))
 
-    def __lshift__(x, y):
+    def __lshift__(self, n):
+        x = long(self)
+        y = long(n)
         return r_uint(x << y)
-    __rlshift__ = __lshift__
 
-    def __rshift__(x, y):
+    def __rlshift__(self, n):
+        y = long(self)
+        x = long(n)
+        return r_uint(x << y)
+
+    def __rshift__(self, n):
+        x = long(self)
+        y = long(n)
         return r_uint(x >> y)
-    __rrshift__ = __rshift__
 
-    def __or__(x, y):
+    def __rrshift__(self, n):
+        y = long(self)
+        x = long(n)
+        return r_uint(x >> y)
+
+    def __or__(self, other):
+        x = long(self)
+        y = long(other)
         return r_uint(x | y)
     __ror__ = __or__
 
-    def __and__(x, y):
+    def __and__(self, other):
+        x = long(self)
+        y = long(other)
         return r_uint(x & y)
     __rand__ = __and__
 
-    def __xor__(x, y):
-        return r_uint(x^y)
+    def __xor__(self, other):
+        x = long(self)
+        y = long(other)
+        return r_uint(x ^ y)
     __rxor__ = __xor__
 
-    def __neg__(x):
+    def __neg__(self):
+        x = long(self)
         return r_uint(-x)
 
     def __pos__(self):
         return r_uint(self)
 
-    def __not__(x):
+    def __invert__(self):
+        x = long(self)
         return r_uint(~x)
 
-    def __pow__(x, y, m=None):
+    def __pow__(self, other, m=None):
+        x = long(self)
+        y = long(other)
         res = pow(x, y, m)
         return r_uint(res)
-    __rpow__ = __pow__
+
+    def __rpow__(self, other, m=None):
+        y = long(self)
+        x = long(other)
+        res = pow(x, y, m)
+        return r_uint(res)
