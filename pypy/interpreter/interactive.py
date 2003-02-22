671b5fc3c5a10ef5a96f4b9c2588176cbabfd893
mwh <mwh@fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada>
2003-02-22 19:12:25 +0000

    module for interactive playing about
    git-svn-id: http://codespeak.net/svn/trunk/src@279 fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada

---
+++ pypy/interpreter/interactive.py
@@ -0,0 +1,19 @@
+from pypy.interpreter import executioncontext
+from pypy.interpreter import pyframe
+from pypy.objspace import trivial
+import code
+
+class PyPyConsole(code.InteractiveConsole):
+    def __init__(self):
+        code.InteractiveConsole.__init__(self)
+        self.space = trivial.TrivialObjSpace()
+        self.ec = executioncontext.ExecutionContext(self.space)
+        self.locals['__builtins__'] = self.space.w_builtins
+
+    def runcode(self, code):
+        # ah ha!
+        frame = pyframe.PyFrame(self.space, code,
+                        self.locals, self.locals)
+        self.ec.eval_frame(frame)
+        
+        
