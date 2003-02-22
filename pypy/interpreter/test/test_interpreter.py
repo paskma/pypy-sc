c7212fe993986c1dcddd302f79e9af50605355c7
arigo <arigo@fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada>
2003-02-22 16:51:40 +0000

    made the test reusable with other object spaces
    git-svn-id: http://codespeak.net/svn/trunk/src@260 fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada

--- pypy/interpreter/test/test_interpreter.py
+++ pypy/interpreter/test/test_interpreter.py
@@ -2,58 +2,57 @@ import unittest
 import testsupport
 
 
-def testcode(code, functionname, args, space=None):
-    """Compile and run the given code string, and then call its function
-    named by 'functionname' with arguments 'args'.
-    The optional 'space' argument can specify an alternate object space."""
-    from interpreter import baseobjspace, executioncontext, appfile
-    if space is None:
-        from objspace.trivial import TrivialObjSpace
-        space = TrivialObjSpace()
-
-    bytecode = compile(code, '<test>', 'exec')
-    apphelper = appfile.AppHelper(space, bytecode)
-    
-    wrappedargs = [space.wrap(arg) for arg in args]
-    try:
-        w_output = apphelper.call(functionname, wrappedargs)
-    except baseobjspace.OperationError, e:
-        print e.w_traceback
-    else:
-        return space.unwrap(w_output)
-
-
 class TestInterpreter(unittest.TestCase):
 
+    def codetest(self, code, functionname, args):
+        """Compile and run the given code string, and then call its function
+        named by 'functionname' with arguments 'args'."""
+        from interpreter import baseobjspace, executioncontext, appfile
+
+        bytecode = compile(code, '<test>', 'exec')
+        apphelper = appfile.AppHelper(self.space, bytecode)
+
+        wrappedargs = [self.space.wrap(arg) for arg in args]
+        try:
+            w_output = apphelper.call(functionname, wrappedargs)
+        except baseobjspace.OperationError, e:
+            print e.w_traceback
+        else:
+            return self.space.unwrap(w_output)
+
+    def setUp(self):
+        from objspace.trivial import TrivialObjSpace
+        self.space = TrivialObjSpace()
+
     def test_trivial(self):
-        x = testcode('''
+        x = self.codetest('''
 def g(): return 42''', 'g', [])
         self.assertEquals(x, 42)
 
     def test_trivial_call(self):
-        x = testcode('''
+        x = self.codetest('''
 def f(): return 42
 def g(): return f()''', 'g', [])
         self.assertEquals(x, 42)
 
     def test_trivial_call2(self):
-        x = testcode('''
+        x = self.codetest('''
 def f(): return 1 + 1
 def g(): return f()''', 'g', [])
         self.assertEquals(x, 2)
 
     def test_print(self):
-        x = testcode('''
+        x = self.codetest('''
 def g(): print 10''', 'g', [])
         self.assertEquals(x, None)
 
     def test_identity(self):
-        x = testcode('''
+        x = self.codetest('''
 def g(x): return x''', 'g', [666])
         self.assertEquals(x, 666)
 
     def test_exception(self):
-        x = testcode('''
+        x = self.codetest('''
 def f():
     try:
         raise Exception, 1
@@ -71,11 +70,11 @@ def f(a):
     finally:
         return 2
 '''
-        self.assertEquals(testcode(code, 'f', [0]), 2)
-        self.assertEquals(testcode(code, 'f', [1]), 2)
+        self.assertEquals(self.codetest(code, 'f', [0]), 2)
+        self.assertEquals(self.codetest(code, 'f', [1]), 2)
 
     def test_raise(self):
-        x = testcode('''
+        x = self.codetest('''
 def f():
     raise 1
 ''', 'f', [])
