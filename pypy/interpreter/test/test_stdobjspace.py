e5840916f7df812bdda21d2fd64036249e907545
arigo <arigo@fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada>
2003-02-22 17:19:39 +0000

    let's debug our standard object space now!
    git-svn-id: http://codespeak.net/svn/trunk/src@264 fd0d7bf2-dfb6-0310-8d31-b7ecfe96aada

---
+++ pypy/interpreter/test/test_stdobjspace.py
@@ -0,0 +1,14 @@
+import unittest
+import testsupport
+import test_interpreter
+
+
+class TestStdObjSpace(test_interpreter.TestInterpreter):
+
+    def setUp(self):
+        from pypy.objspace.std.objspace import StdObjSpace
+        self.space = StdObjSpace()
+
+
+if __name__ == '__main__':
+    unittest.main()
