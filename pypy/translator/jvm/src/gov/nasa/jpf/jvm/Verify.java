d51d472816ee205c12d057aaa7daca22d8ac47ef
Marek Paska <paskma@gmail.com>
2009-05-21 11:20:47 +0200

    jpf.jvm.Verify.random uses ordinary random

--- pypy/translator/jvm/src/gov/nasa/jpf/jvm/Verify.java
+++ pypy/translator/jvm/src/gov/nasa/jpf/jvm/Verify.java
@@ -1,10 +1,20 @@
 package gov.nasa.jpf.jvm;
 
+import java.util.Random;
+
+
 public class Verify
 {
-  public static int random(int max)
-  {
-    return 2;
+  private static Random random;
+  /**
+   * Returns a random number between 0 and max inclusive.
+   */
+  public static int random (int max) {
+    // this is only executed when not running JPF
+    if (random == null) {
+      random = new Random();
+    }
+    return random.nextInt(max + 1);
   }
 
   public static void beginAtomic()
