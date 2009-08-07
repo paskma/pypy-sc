3fc60830912c65e47f5c8ea0889ec31ad35bc5f1
Marek Paska <paskma@gmail.com>
2009-08-07 14:07:53 +0200

    Added foo.py, needed for java.

---
+++ pypy/rpython/module/foo.py
@@ -0,0 +1,44 @@
+
+def bar(x):
+	print "Should not be here."
+	pass
+
+def start_new_thread():
+	print "Should not be here."
+	pass
+
+def allocate_lock():
+	print "Should not be here."
+	return 0
+
+def acquire_lock(lockNum):
+	print "Should not be here."
+	pass
+
+def release_lock(lockNum):
+	print "Should not be here."
+	pass
+
+def acquire_boot_lock():
+	print "Should not be here."
+	pass
+
+def release_boot_lock():
+	print "Should not be here."
+	pass
+
+def dumpln(s):
+	print "Should not be here."
+	pass
+
+def jpf_random(i):
+	print "Should not be here."
+	return 1
+
+def jpf_begin_atomic():
+	print "Should not be here."
+	pass
+
+def jpf_end_atomic():
+	print "Should not be here."
+	pass
